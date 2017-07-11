21825c86b7f602efa9c4c353880afc4977e0f1b1
Rakesh Kumar <rakeshpersonal@gmail.com>
2017-07-11 12:11:25 +0530

    some refinements

--- client/controllers/books.controller.js
+++ client/controllers/books.controller.js
@@ -1,35 +1,37 @@
 var myApp = angular.module('myApp');
 
 function BooksController($scope, $location, $routeParams, BooksService) {
-    var vm  = this;
-    BooksService.getBooks().then(function(data){
+    var vm = this;
+    BooksService.getBooks().then(function (data) {
         vm.books = data;
+    }, function (err) {
+        console.log('SOMETHING GOES WRONG', err)
     });
 
-    if($routeParams.id) {
-        BooksService.getBook($routeParams.id).then(function(data){
+    if ($routeParams.id) {
+        BooksService.getBook($routeParams.id).then(function (data) {
             vm.book = data;
         });
     }
 
-    vm.addBook = function(){
-        BooksService.addBook(vm.book).then(function(data){
+    vm.addBook = function () {
+        BooksService.addBook(vm.book).then(function (data) {
             window.location.href = '#!/';
         });
-    }
-    
-    vm.editBook = function(){
-        BooksService.editBook($routeParams.id, vm.book).then(function(data){
+    };
+
+    vm.editBook = function () {
+        BooksService.editBook($routeParams.id, vm.book).then(function (data) {
             window.location.href = '#!/books/detail/' + $routeParams.id;
         });
-    }
-    
-    vm.deleteBook = function(id){
-        BooksService.deleteBook(id).then(function(data){
+    };
+
+    vm.deleteBook = function (id) {
+        BooksService.deleteBook(id).then(function (data) {
             window.location.href = '#!/books/';
         });
     }
-    
+
 }
 BooksController.$inject = ['$scope', '$location', '$routeParams', 'BooksService'];
-myApp.controller('BooksController', BooksController)
+myApp.controller('BooksController', BooksController);
--- client/services/books.services.js
+++ client/services/books.services.js
@@ -1,42 +1,45 @@
 var myApp = angular.module('myApp');
 
-function BooksService($http) {
-    function getBooks(){
-        return $http.get('/api/books').then(function(response){
+function BooksService($http, $q) {
+    function getBooks() {
+        var deffered = $q.defer();
+        $http.get('/api/books').then(function (response) {
+            deffered.resolve(response.data);
+        }, function (err) {
+            deffered.reject(response.data);
+            console.error('ERROR: ', err.status)
+        });
+        return deffered.promise;
+    }
+
+    function getBook(id) {
+        return $http.get('/api/books/' + id).then(function (response) {
             return response.data
-        }, function(err) {
+        }, function (err) {
             console.error('ERROR: ', err.status)
         });
     }
-    
-    function getBook(id){
-        return $http.get('/api/books/' + id).then(function(response){
-            return response.data
-        }, function(err) {
-            console.error('ERROR: ', err.status)
-        });
-    }
-    
-    function addBook(newBook){
-        return $http.post('/api/books/', newBook).then(function(response){
+
+    function addBook(newBook) {
+        return $http.post('/api/books/', newBook).then(function (response) {
             console.info('Added Successfully:', response)
-        }, function(err){
+        }, function (err) {
             console.error('ERROR:', err)
         });
     }
-    
-    function editBook(id, newBook){
-        return $http.put('/api/books/'+id, newBook).then(function(response){
+
+    function editBook(id, newBook) {
+        return $http.put('/api/books/' + id, newBook).then(function (response) {
             console.info('Updated Successfully:', response.status, response.statusText)
-        }, function(err){
+        }, function (err) {
             console.error('ERROR:', err)
         });
     }
-    
-    function deleteBook(id){
-        return $http.delete('/api/books/'+id).then(function(response){
+
+    function deleteBook(id) {
+        return $http.delete('/api/books/' + id).then(function (response) {
             console.info('Delete Successfully:', response.status, response.statusText)
-        }, function(err){
+        }, function (err) {
             console.error('ERROR:', err)
         });
     }
@@ -49,5 +52,6 @@ function BooksService($http) {
         deleteBook
     }
 }
-BooksService.$inject = ['$http'];
-myApp.service('BooksService', BooksService)
+BooksService.$inject = ['$http', '$q'];
+myApp.service('BooksService', BooksService);
+// myApp.factory('BooksService', BooksService);
