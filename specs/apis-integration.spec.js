b09848d0e2c8c58b01621d78983483c4b635e8cd
Rakesh Kumar <rakeshpersonal@gmail.com>
2017-08-21 14:40:37 +0530

    refactor integration test cases

--- specs/apis-integration.spec.js
+++ specs/apis-integration.spec.js
@@ -7,7 +7,9 @@ const request = supertest(app);
 const headerEnums = require('./../app/server/enums/headersEnum');
 const statusCode = require('./../app/server/enums/statusCodes');
 
-const userReportId = 'c9f9efa06c09474090d9caeacbf8d0da';
+let userReportId;
+let fromDate = 20100320; // Date format: YYYYMMDD
+let endDate = 20151231;  // Date format: YYYYMMDD
 
 // Test suit for APIs
 describe('Thesys Cat express app', () => {
@@ -15,42 +17,52 @@ describe('Thesys Cat express app', () => {
     it('should responds 401 unauthorized to /thesys/api/reporters/ if missing requested headers', (done) => {
         request.get('/thesys/api/reporters/')
             .set({})
-            .expect(statusCode.UNAUTHORIZED).end(onEndTestcase(done));
+            .expect(statusCode.UNAUTHORIZED)
+            .end(onEndTestcase(done));
     });
 
     it('should responds 200 success to /thesys/api/reporters/ with valid requested headers', (done) => {
         request.get('/thesys/api/reporters/')
             .set(headerEnums.reports)
-            .expect(statusCode.SUCCESS_OK).end(onEndTestcase(done));
+            .expect(res => {
+                userReportId = res.body[0].id
+            })
+            .expect(statusCode.SUCCESS_OK)
+            .end(onEndTestcase(done))
     });
 
     it('should responds 401 unauthorized to /thesys/api/reporters/:id if missing requested headers', (done) => {
         request.get('/thesys/api/reporters/' + userReportId)
             .set({})
-            .expect(statusCode.UNAUTHORIZED).end(onEndTestcase(done));
+            .expect(statusCode.UNAUTHORIZED)
+            .end(onEndTestcase(done));
     });
 
     it('should responds 200 success to /thesys/api/reporters/:id with valid requested headers', (done) => {
         request.get('/thesys/api/reporters/' + userReportId)
             .set(headerEnums.reports)
-            .expect(statusCode.SUCCESS_OK).end(onEndTestcase(done));
+            .expect(statusCode.SUCCESS_OK)
+            .end(onEndTestcase(done));
     });
 
-    it('should responds 401 unauthorized to /thesys/api/reporters/:id/filter?fromDate=20100320&endDate=20151231 if missing requested headers', (done) => {
-        request.get('/thesys/api/reporters/' + userReportId + '/filter?fromDate=20100320&endDate=20151231')
+    it(`should responds 401 unauthorized to /thesys/api/reporters/:id/filter?fromDate=${fromDate}&endDate=${endDate} if missing requested headers`, (done) => {
+        request.get(`/thesys/api/reporters/${userReportId}/filter?fromDate=${fromDate}&endDate=${endDate}`)
             .set({})
-            .expect(statusCode.UNAUTHORIZED).end(onEndTestcase(done));
+            .expect(statusCode.UNAUTHORIZED)
+            .end(onEndTestcase(done));
     });
 
-    it('should responds 200 success to /thesys/api/reporters/:id/filter?fromDate=20100320&endDate=20151231 with valid requested headers', (done) => {
-        request.get('/thesys/api/reporters/' + userReportId + '/filter?fromDate=20100320&endDate=20151231')
+    it(`should responds 200 success to /thesys/api/reporters/:id/filter?fromDate=${fromDate}&endDate=${endDate} with valid requested headers`, (done) => {
+        request.get(`/thesys/api/reporters/${userReportId}/filter?fromDate=${fromDate}&endDate=${endDate}`)
             .set(headerEnums.reports)
-            .expect(statusCode.SUCCESS_OK).end(onEndTestcase(done));
+            .expect(statusCode.SUCCESS_OK)
+            .end(onEndTestcase(done));
     });
 
     it('should responds 404 everything else', (done) => {
         request.get('/')
             .set(headerEnums.reports)
-            .expect(statusCode.DATA_NOT_FOUND).end(onEndTestcase(done));
+            .expect(statusCode.DATA_NOT_FOUND)
+            .end(onEndTestcase(done));
     });
 });
